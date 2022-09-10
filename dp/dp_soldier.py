1ba797949632e7491713cfa8f10ff13b5e5c6993
jaeyoonheo <109254266+jaeyoonheo@users.noreply.github.com>
2022-09-10 19:21:10 +0900

    Create dp_soldier.py

---
+++ dp/dp_soldier.py
@@ -0,0 +1,13 @@
+n = int(input())
+soldier = list(map(int, input().split()))
+
+soldier.revers()
+
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(0, i):
+        if soldier[j] < soldier[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
