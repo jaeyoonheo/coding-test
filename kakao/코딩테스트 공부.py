13375b3d28d23c65ecefea29160d4e839c08d7b8
jaeyoonheo <109254266+jaeyoonheo@users.noreply.github.com>
2022-11-15 06:11:28 +0900

    Update 코딩테스트 공부.py

--- kakao/코딩테스트 공부.py
+++ kakao/코딩테스트 공부.py
@@ -1,24 +1,25 @@
-def solution(alp, cop, problems):
+def solution(alp,cop,problems):
     answer = 0
+    alp_target = 0
+    cop_target = 0
     
-    alp_target = max([i[0] for i in problems])
-    cop_target = max([i[1] for i in problems])
+    for problem in problems:
+        alp_target = max(alp_target, problem[0])
+        cop_target = max(cop_target, problem[1])
     
-    dp = [[0]*(cop_target+1) for _ in range(alp_target+1)]
+    dp = [[float('inf')]*(cop_target+1) for _ in range(alp_target+1)]
+    dp[alp][cop] = 0
     
-    for i in range(alp_target+1):
-        for j in range(cop_target+1):
-            if i < 
-            dp[i+1][j] = min(dp[i+1][j], dp[i][j]+1)
-            dp[i][j+1] = min(dp[i][j+1], dp[i][j]+1)
-            for problem in problems:
-                if alp < problem[0]:
-                    continue:
-                if cop < pronlem[1]:
-                    continue:
-                
-                dp[i+problem[2]][j+problem[3]] = min(dp[i+problem[2]][j+problem[3]], dp[i][j]+problem[4])
-                
-    answer = 
+    for i in range(alp, alp_target+1):
+        for j in range(cop, cop_target+1):
+            if i < alp_target:
+                dp[i+1][j] = min(dp[i+1][j], dp[i][j]+1)
+            if j < cop_target:
+                dp[i][j+1] = min(dp[i][j+1], dp[i][j]+1)
+            for alp_req, cop_req, alp_rwd, cop_rwd, time in problems:
+                if i >= alp_req and j >= cop_req:                  
+                    i_ = min(i+alp_rwd,alp_target)
+                    j_ = min(j+cop_rwd,cop_target)
+                    dp[i_][j_] = min(dp[i_][j_], dp[i][j] + time)
     
-    return answer
+    return dp[alp_target][cop_target]
